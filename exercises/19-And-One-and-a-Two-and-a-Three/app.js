870f677f9befe98b99a5514ae16e4001b2eafd7a
leisyarcia <leisyarcia@gmail.com>
2021-01-29 19:37:24 +0000

    19 not passing

--- exercises/19-And-One-and-a-Two-and-a-Three/app.js
+++ exercises/19-And-One-and-a-Two-and-a-Three/app.js
@@ -3,14 +3,13 @@ contact = {
     phone: "321-321-4321",
     email: "test@test.com"
 }
-// let result=[];
+// let result={};
 // for(let key in contact){
 //     // Code goes here
-//     result.push (key + " : " + contact[key])
+//     result= (key + " : " + contact[key])
 //     console.log(result);
+   
 // }
-for(key in contact){
-
- 
+for(key in contact){ 
   console.log(`${key}: ${contact[key]}`);
-}
+};
